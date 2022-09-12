5c16afc31a4925680718711070ce8dcbdf9433ad
the-weeex <thekurco@gmail.com>
2022-09-11 22:53:16 -0300

    Agregar archivos de proyecto.

---
+++ 1er Ejercicio/Nombre y Hora/Program.cs
@@ -0,0 +1,11 @@
+﻿// Nombre
+Console.WriteLine("Escriba su nombre por favor:");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Hola, " + nombre + "!!!!");
+
+//Hora
+Console.WriteLine("¿Que hora es?");
+string hora = Console.ReadLine();
+
+Console.WriteLine("Es la hora: "+ hora);
