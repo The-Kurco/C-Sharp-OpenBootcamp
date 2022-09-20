c1737d8ccfdd2e496b784dcd2f30b72a9e0aec47
the-weeex <thekurco@gmail.com>
2022-09-19 21:32:52 -0300

    IF

---
+++ 5to Ejercicio/Bucles II/Program.cs
@@ -0,0 +1,26 @@
+﻿// IF
+
+Console.WriteLine("Hola, el precio de la mesa es de $20");
+Console.WriteLine("Ingrese sus datos por favor");
+Console.WriteLine("Nombre:");
+string name= Console.ReadLine();
+
+Console.WriteLine("Email:");
+string email = Console.ReadLine();
+
+Console.WriteLine("Cupon de descuento:");
+string cupon = Console.ReadLine();
+
+const int producto = 20;
+int precioFinal = 0;
+string cuponPermitido = "HOLAMUNDO";
+
+if (cupon == cuponPermitido)
+{
+    precioFinal = producto - 5;
+    Console.WriteLine($"El precio total de la mesa con el descuento aplicado es de: ${precioFinal}");
+}
+else if (cupon != cuponPermitido)
+{
+    Console.WriteLine($"El precio total de la mesa es de: ${producto}");
+}
